57b8fc9644b764f005d8fae6d775298ff2e616d8
Tharunrajravi <145248134+Tharunrajravi@users.noreply.github.com>
2025-12-26 19:28:45 +0530

    Add files via upload

---
+++ network_commands.sh
@@ -0,0 +1,12 @@
+# Get the IP address of a domain using DNS
+nslookup guvi.in
+# Test network connectivity to the domain
+ping guvi.in
+# Display real-time CPU and memory usage
+htop
+# netcat command to check whether the port is open or not
+nc -zv guvi.com 9000
+# telnet command
+telnet guvi.com 9000
+#nmap command
+nmap -p 9000 guvi.com
